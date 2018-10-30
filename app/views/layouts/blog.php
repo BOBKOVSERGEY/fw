6a4cde23322ad139c1983a849e66868fcc46e342
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-10-30 15:37:32 +0300

    Мультиязычность

--- app/views/layouts/blog.php
+++ app/views/layouts/blog.php
@@ -3,6 +3,7 @@
 <head>
   <?php \fw\core\base\View::getMeta();?>
   <link href="/blog/css/bootstrap.css" rel='stylesheet' type='text/css' />
+  <link href="/bootstrap/bootstrap.min.css" rel='stylesheet' type='text/css' />
   <link href="/blog/css/style.css" rel='stylesheet' type='text/css' />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
